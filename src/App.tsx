249622a7e2534607a7f5d49c74e6b31964a628c1
Itai Fish <chessandpotatoes@gmail.com>
2023-09-29 22:51:28 -0400

    add joy ui library

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
 import { invoke } from '@tauri-apps/api/tauri';
-import './App.css';
 
 function App() {
 	const [greetMsg, setGreetMsg] = useState('');
@@ -15,18 +13,6 @@ function App() {
 	return (
 		<div className="container">
 			<h1>Welcome to Tauri!</h1>
-
-			<div className="row">
-				<a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-					<img src="/vite.svg" className="logo vite" alt="Vite logo" />
-				</a>
-				<a href="https://tauri.app" target="_blank" rel="noreferrer">
-					<img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-				</a>
-				<a href="https://reactjs.org" target="_blank" rel="noreferrer">
-					<img src={reactLogo} className="logo react" alt="React logo" />
-				</a>
-			</div>
 
 			<p>Click on the Tauri, Vite, and React logos to learn more.</p>
 
