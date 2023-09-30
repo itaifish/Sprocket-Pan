176466776e31ee4aa256f525bbdf551ad4bde65c
Itai Fish <chessandpotatoes@gmail.com>
2023-09-29 23:11:11 -0400

    update tsconfig

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles.css';
 import '@fontsource/inter';
+import { StyledEngineProvider } from '@mui/joy/styles';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
-		<App />
+		<StyledEngineProvider injectFirst>
+			<App />
+		</StyledEngineProvider>
 	</React.StrictMode>,
 );
