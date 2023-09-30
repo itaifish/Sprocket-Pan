80f0c10fac0688f275c05a84f5e5d36e8dd6eed1
Itai Fish <chessandpotatoes@gmail.com>
2023-09-30 00:02:42 -0400

    styles

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,13 @@ import App from './App';
 import './styles.css';
 import '@fontsource/inter';
 import { CssVarsProvider, StyledEngineProvider } from '@mui/joy/styles';
+import { CssBaseline } from '@mui/joy';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
 		<StyledEngineProvider injectFirst>
 			<CssVarsProvider disableTransitionOnChange>
+				<CssBaseline />
 				<App />
 			</CssVarsProvider>
 		</StyledEngineProvider>
