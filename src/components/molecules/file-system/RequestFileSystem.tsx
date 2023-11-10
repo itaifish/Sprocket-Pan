d77395e8eaa2d9ae1af8ff9b6fb57c8473921222
Itai Fish <chessandpotatoes@gmail.com>
2023-11-09 23:35:14 -0500

    can duplicate and delete requets too now

--- src/components/molecules/file-system/RequestFileSystem.tsx
+++ src/components/molecules/file-system/RequestFileSystem.tsx
@@ -1,30 +1,93 @@
-import { ListItem, ListItemButton, ListItemDecorator, ListSubheader } from '@mui/joy';
+import {
+	Dropdown,
+	IconButton,
+	ListItem,
+	ListItemButton,
+	ListItemDecorator,
+	ListSubheader,
+	Menu,
+	MenuButton,
+	MenuItem,
+} from '@mui/joy';
 import { EndpointRequest } from '../../../types/application-data/application-data';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import TextSnippetIcon from '@mui/icons-material/TextSnippet';
 import { ApplicationDataContext, TabsContext } from '../../../App';
 import { keepStringLengthReasonable } from '../../../utils/string';
 import { tabsManager } from '../../../managers/TabsManager';
+import { MoreVert } from '@mui/icons-material';
+import { applicationDataManager } from '../../../managers/ApplicationDataManager';
+import FolderCopyIcon from '@mui/icons-material/FolderCopy';
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
+import { AreYouSureModal } from '../../atoms/modals/AreYouSureModal';
 
 export function RequestFileSystem({ request }: { request: EndpointRequest }) {
 	const tabsContext = useContext(TabsContext);
 	const { tabs } = tabsContext;
 	const data = useContext(ApplicationDataContext);
+	const [menuOpen, setMenuOpen] = useState(false);
 	const endpointData = data.endpoints[request.endpointId];
+	const [deleteModalOpen, setDeleteModalOpen] = useState(false);
+
+	const menuButton = (
+		<>
+			<Dropdown open={menuOpen} onOpenChange={(_event, isOpen) => setMenuOpen(isOpen)}>
+				<MenuButton slots={{ root: IconButton }} slotProps={{ root: { variant: 'plain', color: 'neutral' } }}>
+					<MoreVert />
+				</MenuButton>
+				<Menu>
+					<MenuItem
+						onClick={() => {
+							setMenuOpen(false);
+							applicationDataManager.addNew('request', { endpointId: request.endpointId }, request);
+						}}
+					>
+						<ListItemDecorator>
+							<IconButton aria-label="copy endpoint" size="sm">
+								<FolderCopyIcon fontSize="small" />
+							</IconButton>
+							Duplicate
+						</ListItemDecorator>
+					</MenuItem>
+					<MenuItem
+						onClick={() => {
+							setMenuOpen(false);
+							setDeleteModalOpen(true);
+						}}
+					>
+						<ListItemDecorator>
+							<IconButton aria-label="delete endpoint" size="sm">
+								<DeleteForeverIcon fontSize="small" />
+							</IconButton>
+							Delete
+						</ListItemDecorator>
+					</MenuItem>
+				</Menu>
+			</Dropdown>
+		</>
+	);
 	return (
-		<ListItem nested>
-			<ListItemButton
-				onClick={() => {
-					tabsManager.selectTab(tabsContext, request.id, 'request');
-				}}
-				selected={tabs.selected === request.id}
-				color={endpointData.defaultRequest === request.id ? 'primary' : 'neutral'}
-			>
-				<ListItemDecorator>
-					<TextSnippetIcon fontSize="small" />
-				</ListItemDecorator>
-				<ListSubheader>{keepStringLengthReasonable(request.name)}</ListSubheader>
-			</ListItemButton>
-		</ListItem>
+		<>
+			<ListItem nested endAction={<>{menuButton}</>}>
+				<ListItemButton
+					onClick={() => {
+						tabsManager.selectTab(tabsContext, request.id, 'request');
+					}}
+					selected={tabs.selected === request.id}
+					color={endpointData.defaultRequest === request.id ? 'primary' : 'neutral'}
+				>
+					<ListItemDecorator>
+						<TextSnippetIcon fontSize="small" />
+					</ListItemDecorator>
+					<ListSubheader>{keepStringLengthReasonable(request.name)}</ListSubheader>
+				</ListItemButton>
+			</ListItem>
+			<AreYouSureModal
+				action={`delete '${request.name}' and all its data`}
+				open={deleteModalOpen}
+				closeFunc={() => setDeleteModalOpen(false)}
+				actionFunc={() => applicationDataManager.delete('request', request.id, tabsContext)}
+			/>
+		</>
 	);
 }
