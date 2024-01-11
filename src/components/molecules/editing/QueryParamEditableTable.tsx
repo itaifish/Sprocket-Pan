00d70e813d4d86dce1b2dc38a60bda58a7c05557
Itai Fish <chessandpotatoes@gmail.com>
2024-01-11 17:20:14 -0500

    fixed some bugs

--- src/components/molecules/editing/QueryParamEditableTable.tsx
+++ src/components/molecules/editing/QueryParamEditableTable.tsx
@@ -20,6 +20,7 @@ export function QueryParamEditableTable(props: QueryParamEditableTableProps) {
 	useEffect(() => {
 		const data = QueryParamUtils.toTableData<QueryParams>(localDataState);
 		setDisplayData(data);
+		log.info(JSON.stringify(localDataState));
 	}, [localDataState]);
 
 	const changeData = (id: number, newKey?: string, newValue?: string) => {
