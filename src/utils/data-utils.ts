afd9bb968c4c79b2f9ab8ebe80352e5655dedc6a
Itai Fish <chessandpotatoes@gmail.com>
2024-01-13 00:33:16 -0500

    getting there

--- src/utils/data-utils.ts
+++ src/utils/data-utils.ts
@@ -1,4 +1,5 @@
 import { QueryParams } from '../types/application-data/application-data';
+import { log } from './logging';
 
 export class QueryParamUtils {
 	private constructor() {}
@@ -15,19 +16,20 @@ export class QueryParamUtils {
 	static updateValue = (queryParams: QueryParams, dataId: number, newValue: string) => {
 		const datumToUpdate = queryParams.__data[dataId];
 		const updateIndex = queryParams[datumToUpdate.key].findIndex((x) => x == datumToUpdate.value);
-		if (updateIndex) {
+		if (updateIndex != undefined && updateIndex >= 0) {
 			queryParams[datumToUpdate.key][updateIndex] = newValue;
 		}
 		datumToUpdate.value = newValue;
 	};
 
 	static updateKey = (queryParams: QueryParams, dataId: number, newKey: string, valueId?: number) => {
+		log.info(JSON.stringify({ dataId, newKey, valueId }));
 		const datumToUpdate = queryParams.__data[dataId];
 		if (!datumToUpdate) {
 			return;
 		}
 		const updateIndex = valueId ?? queryParams[datumToUpdate.key].findIndex((x) => x === datumToUpdate.value);
-		if (updateIndex) {
+		if (updateIndex != undefined && updateIndex >= 0) {
 			queryParams[datumToUpdate.key].splice(updateIndex, 1);
 			if (queryParams[datumToUpdate.key].length == 0) {
 				delete queryParams[datumToUpdate.key];
@@ -38,7 +40,6 @@ export class QueryParamUtils {
 				queryParams[newKey] = [datumToUpdate.value];
 			}
 			datumToUpdate.key = newKey;
-			this.deleteKeyValuePair(queryParams, dataId);
 		}
 	};
 
