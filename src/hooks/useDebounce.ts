c0d0a90dd292b9c29a6dbb546e1a991eac83d674
Itai Fish <chessandpotatoes@gmail.com>
2024-01-14 00:52:20 -0500

    add eventing to the debounce and upgrade search bar

--- src/hooks/useDebounce.ts
+++ src/hooks/useDebounce.ts
@@ -1,19 +1,22 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { Constants } from '../utils/constants';
+import { EventEmitter } from '@tauri-apps/api/shell';
 
 interface UseDebounceProps<T> {
 	state: T;
 	setState: React.Dispatch<React.SetStateAction<T>> | ((newState: T) => void);
+	debounceOverride?: number;
 }
 
 export const useDebounce = <TData>(props: UseDebounceProps<TData>) => {
 	const [localDataState, setLocalDataState] = useState<TData>(props.state);
 	const [typingBufferTimeout, setTypingBufferTimeout] = useState<null | NodeJS.Timeout>(null);
-
+	const debounceEventEmitter = useMemo(() => new EventEmitter<'sync' | 'desync'>(), []);
 	// When the state changes, set the local state to the state
 	useEffect(() => {
 		if (JSON.stringify(localDataState) !== JSON.stringify(props.state)) {
 			setLocalDataState(structuredClone(props.state));
+			debounceEventEmitter.emit('sync');
 		}
 	}, [props.state]);
 	// when the local state changes, if it is different from the state, update the state
@@ -25,10 +28,12 @@ export const useDebounce = <TData>(props: UseDebounceProps<TData>) => {
 		const timeout = setTimeout(() => {
 			if (JSON.stringify(localDataState) !== JSON.stringify(props.state)) {
 				props.setState(localDataState);
+				debounceEventEmitter.emit('sync');
 			}
-		}, Constants.debounceTimeMS);
+		}, props.debounceOverride ?? Constants.debounceTimeMS);
 		setTypingBufferTimeout(timeout);
+		debounceEventEmitter.emit('desync');
 	}, [localDataState]);
 
-	return { localDataState, setLocalDataState };
+	return { localDataState, setLocalDataState, debounceEventEmitter };
 };
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,5 @@
 export const Constants = {
+	searchDebounceTimeMS: 300,
 	debounceTimeMS: 500,
 	scriptsTimeoutMS: 5_000,
 	networkRequestTimeoutMS: 10_000,
