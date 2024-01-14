748abb37e501ef6a67761832fe1c5a08a2f1adbc
itaifish <chessandpotatoes@gmail.com>
2024-01-14 01:04:20 -0500

    debounce bitch (#13)
    * Move search to float
    
    * fix bug with display env vars
    
    * fixed envs, gotta also fix query params
    
    * adding useDebounce
    
    * lots of progress
    
    * fixed some bugs
    
    * WIP queryParam update
    
    * lets try it
    
    * getting there
    
    * lots more progress :-)
    
    * yay looks good
    
    * add eventing to the debounce and upgrade search bar

--- src/components/atoms/SearchInputField.tsx
+++ src/components/atoms/SearchInputField.tsx
@@ -1,5 +1,10 @@
 import SearchRoundedIcon from '@mui/icons-material/SearchRounded';
 import { Input } from '@mui/joy';
+import { useDebounce } from '../../hooks/useDebounce';
+import { useState } from 'react';
+import PendingOutlinedIcon from '@mui/icons-material/PendingOutlined';
+import SavedSearchIcon from '@mui/icons-material/SavedSearch';
+import { Constants } from '../../utils/constants';
 
 export function SearchInputField({
 	searchText,
@@ -8,18 +13,43 @@ export function SearchInputField({
 	searchText: string;
 	setSearchText: React.Dispatch<React.SetStateAction<string>>;
 }) {
+	const { localDataState, setLocalDataState, debounceEventEmitter } = useDebounce({
+		state: searchText,
+		setState: setSearchText,
+		debounceOverride: Constants.searchDebounceTimeMS,
+	});
+	const [isTyping, setTyping] = useState(false);
+	debounceEventEmitter.on('sync', () => {
+		if (isTyping) {
+			setTyping(false);
+		}
+	});
+	debounceEventEmitter.on('desync', () => {
+		if (!isTyping) {
+			setTyping(true);
+		}
+	});
 	return (
 		<Input
 			size="sm"
 			variant="outlined"
 			placeholder="Search for something"
 			startDecorator={<SearchRoundedIcon color="primary" />}
+			endDecorator={
+				searchText === '' && localDataState === '' ? (
+					<></>
+				) : isTyping ? (
+					<PendingOutlinedIcon color="secondary" />
+				) : (
+					<SavedSearchIcon color="action" />
+				)
+			}
 			sx={{
 				boxShadow: 'sm',
 				width: '100%',
 			}}
-			value={searchText}
-			onChange={(e) => setSearchText(e.target.value)}
+			value={localDataState}
+			onChange={(e) => setLocalDataState(e.target.value)}
 		/>
 	);
 }
