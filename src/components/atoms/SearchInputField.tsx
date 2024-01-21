9c4c34a3f9a46f4551f6d484230207592d8f7209
Itai Fish <chessandpotatoes@gmail.com>
2024-01-21 18:38:32 -0500

    add cledar button to search

--- src/components/atoms/SearchInputField.tsx
+++ src/components/atoms/SearchInputField.tsx
@@ -1,10 +1,11 @@
 import SearchRoundedIcon from '@mui/icons-material/SearchRounded';
-import { Input } from '@mui/joy';
+import { IconButton, Input, Stack } from '@mui/joy';
 import { useDebounce } from '../../hooks/useDebounce';
 import { useState } from 'react';
 import PendingOutlinedIcon from '@mui/icons-material/PendingOutlined';
 import SavedSearchIcon from '@mui/icons-material/SavedSearch';
 import { Constants } from '../../utils/constants';
+import ClearIcon from '@mui/icons-material/Clear';
 
 export function SearchInputField({
 	searchText,
@@ -30,26 +31,41 @@ export function SearchInputField({
 		}
 	});
 	return (
-		<Input
-			size="sm"
-			variant="outlined"
-			placeholder="Search for something"
-			startDecorator={<SearchRoundedIcon color="primary" />}
-			endDecorator={
-				searchText === '' && localDataState === '' ? (
-					<></>
-				) : isTyping ? (
-					<PendingOutlinedIcon color="secondary" />
-				) : (
-					<SavedSearchIcon color="action" />
-				)
-			}
-			sx={{
-				boxShadow: 'sm',
-				width: '100%',
-			}}
-			value={localDataState}
-			onChange={(e) => setLocalDataState(e.target.value)}
-		/>
+		<>
+			<Stack direction={'row'}>
+				<Input
+					size="sm"
+					variant="outlined"
+					placeholder="Search for something"
+					startDecorator={<SearchRoundedIcon color="primary" />}
+					endDecorator={
+						searchText === '' && localDataState === '' ? (
+							<></>
+						) : isTyping ? (
+							<PendingOutlinedIcon color="secondary" />
+						) : (
+							<SavedSearchIcon color="action" />
+						)
+					}
+					sx={{
+						boxShadow: 'sm',
+						width: '100%',
+					}}
+					value={localDataState}
+					onChange={(e) => setLocalDataState(e.target.value)}
+				/>
+				<IconButton
+					size="sm"
+					variant="plain"
+					color={'danger'}
+					onClick={() => {
+						setLocalDataState('');
+						setTyping(false);
+					}}
+				>
+					<ClearIcon />
+				</IconButton>
+			</Stack>
+		</>
 	);
 }
