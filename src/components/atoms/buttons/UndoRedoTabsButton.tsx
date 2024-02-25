dc4070aab837815d661713143ec1c35dcddebd8a
Itai Fish <chessandpotatoes@gmail.com>
2024-02-25 01:55:18 -0500

    undo redo tab selection works

---
+++ src/components/atoms/buttons/UndoRedoTabsButton.tsx
@@ -0,0 +1,44 @@
+import UndoRoundedIcon from '@mui/icons-material/UndoRounded';
+import RedoRoundedIcon from '@mui/icons-material/RedoRounded';
+import { Stack, IconButton } from '@mui/joy';
+import { useContext, useEffect, useState } from 'react';
+import { tabsManager } from '../../../managers/TabsManager';
+import { TabsContext } from '../../../managers/GlobalContextManager';
+import { log } from '../../../utils/logging';
+
+export function UndoRedoTabsButton() {
+	const [goBackIndex, setGoBackIndex] = useState<number | null>(null);
+	const [goForwardIndex, setGoForwardIndex] = useState<number | null>(null);
+	const tabsContext = useContext(TabsContext);
+
+	useEffect(() => {
+		const onTabSelected = () => {
+			setGoBackIndex(tabsManager.peekHistoryPrevious());
+			setGoForwardIndex(tabsManager.peekHistoryNext());
+			log.info('Tab Selected');
+		};
+		tabsManager.on('TabSelected', onTabSelected);
+		return () => {
+			tabsManager.removeListener('TabSelected', onTabSelected);
+		};
+	}, []);
+
+	return (
+		<Stack direction={'row'} spacing={0} justifyContent={'flex-end'}>
+			<IconButton
+				variant="outlined"
+				disabled={goBackIndex == null}
+				onClick={() => goBackIndex != null && tabsManager.selectTabFromHistory(tabsContext, goBackIndex)}
+			>
+				<UndoRoundedIcon />
+			</IconButton>
+			<IconButton
+				variant="outlined"
+				disabled={goForwardIndex == null}
+				onClick={() => goForwardIndex != null && tabsManager.selectTabFromHistory(tabsContext, goForwardIndex)}
+			>
+				<RedoRoundedIcon />
+			</IconButton>
+		</Stack>
+	);
+}
--- src/managers/TabsManager.ts
+++ src/managers/TabsManager.ts
@@ -1,19 +1,72 @@
+import { EventEmitter } from '@tauri-apps/api/shell';
 import { ApplicationData } from '../types/application-data/application-data';
 import { TabType } from '../types/state/state';
 import { TabsContextType } from './GlobalContextManager';
 
-class TabsManager {
+type TabHistoryElement = {
+	type: TabType;
+	id: string;
+};
+
+type TabEvent = 'TabSelected';
+class TabsManager extends EventEmitter<TabEvent> {
 	public static readonly INSTANCE = new TabsManager();
 
-	private constructor() {}
+	private history: TabHistoryElement[] = [];
+	private historyLocation: number = 0;
 
-	selectTab(tabContext: TabsContextType, tabId: string, tabType: TabType) {
+	private constructor() {
+		super();
+	}
+
+	/**
+	 * This function returns in the index of the next item in the history, if it exists, null otherwise
+	 * @returns int representing the index of the next location, if it exists
+	 */
+	peekHistoryNext() {
+		if (this.historyLocation == this.history.length - 1) {
+			return null;
+		}
+		return this.historyLocation + 1;
+	}
+
+	/**
+	 * This function returns in the index of the previous item in the history, if it exists, null otherwise
+	 * @returns int representing the index of the previous location, if it exists
+	 */
+	peekHistoryPrevious() {
+		if (this.historyLocation == 0) {
+			return null;
+		}
+		return this.historyLocation - 1;
+	}
+
+	selectTabFromHistory(tabContext: TabsContextType, reselectIndex: number) {
+		const tabData = this.history[reselectIndex];
+		if (tabData != undefined) {
+			this.selectTab(tabContext, tabData.id, tabData.type, reselectIndex);
+		}
+	}
+
+	selectTab(tabContext: TabsContextType, tabId: string, tabType: TabType, reselectIndex?: number) {
 		const { tabs, setTabs } = tabContext;
 		if (tabs.tabs[tabId] != null) {
 			setTabs({ tabs: tabs.tabs, selected: tabId });
 		} else {
 			setTabs({ tabs: { ...tabs.tabs, [tabId]: tabType }, selected: tabId });
 		}
+		if (reselectIndex == undefined) {
+			if (tabId != this.history[this.historyLocation]?.id) {
+				this.history.splice(this.historyLocation + 1, Infinity, {
+					type: tabType,
+					id: tabId,
+				});
+				this.historyLocation = this.history.length - 1;
+			}
+		} else {
+			this.historyLocation = reselectIndex;
+		}
+		this.emit('TabSelected');
 	}
 
 	closeTab(tabContext: TabsContextType, tabId: string) {
