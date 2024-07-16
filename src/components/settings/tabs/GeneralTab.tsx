f17da744511f8ddf7790ac263734c74875dec836
Itai Fish <chessandpotatoes@gmail.com>
2024-07-16 15:22:02 -0400

    fix centering

--- src/components/settings/tabs/GeneralTab.tsx
+++ src/components/settings/tabs/GeneralTab.tsx
@@ -1,4 +1,4 @@
-import { Select, Stack, Option, FormControl, FormLabel, Button, CircularProgress, Box } from '@mui/joy';
+import { Select, Stack, Option, FormControl, FormLabel, Button, CircularProgress } from '@mui/joy';
 import ZoomInIcon from '@mui/icons-material/ZoomIn';
 import { InputSlider } from '../../shared/input/InputSlider';
 import { Settings } from '../../../types/settings/settings';
@@ -97,18 +97,16 @@ export function GeneralTab({ settings, setSettings }: SettingsTabProps) {
 					sx={{ maxWidth: '300px' }}
 				>
 					Check for update
-				</Button>{' '}
-				<Box sx={{ transform: 'scale(1.5)' }}>
-					{hasCheckedForUpdate ? (
-						<SprocketTooltip text="You have already checked for updates">
-							<CloudDoneIcon color="success" />
-						</SprocketTooltip>
-					) : (
-						<SprocketTooltip text="You have not yet checked for updates">
-							<HelpIcon color="primary" />
-						</SprocketTooltip>
-					)}
-				</Box>
+				</Button>
+				{hasCheckedForUpdate ? (
+					<SprocketTooltip text="You have already checked for updates">
+						<CloudDoneIcon sx={{ transform: 'scale(1.4)' }} color="success" />
+					</SprocketTooltip>
+				) : (
+					<SprocketTooltip text="You have not yet checked for updates">
+						<HelpIcon sx={{ transform: 'scale(1.4)' }} color="primary" />
+					</SprocketTooltip>
+				)}
 			</Stack>
 		</Stack>
 	);
