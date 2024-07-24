b2402e45531186b86871cd0b68d57f08ad8db037
Itai Fish <chessandpotatoes@gmail.com>
2024-07-23 22:28:46 -0400

    2.5s debounce -> 1.5s debounce

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,7 +1,7 @@
 export const Constants = {
 	searchDebounceTimeMS: 300,
 	debounceTimeMS: 500,
-	longEditTimeMS: 2_500,
+	longEditTimeMS: 1_500,
 	scriptsTimeoutMS: 5_000,
 	networkRequestTimeoutMS: 10_000,
 	minimumScriptRunTimeMS: 400,
