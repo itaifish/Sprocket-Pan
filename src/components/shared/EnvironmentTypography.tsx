f0aac2de106d7e017c4ecab8128a71e21b9e6ce1
KateLynn Pullen <kdpullen95@gmail.com>
2024-11-30 09:30:09 -0500

    it turns out the reason it wasn't working is I didn't finish it

--- src/components/shared/EnvironmentTypography.tsx
+++ src/components/shared/EnvironmentTypography.tsx
@@ -1,5 +1,7 @@
 import { Typography, TypographyProps } from '@mui/joy';
 import { Snippet } from '../../managers/EnvironmentContextResolver';
+import { selectSettings } from '../../state/active/selectors';
+import { useSelector } from 'react-redux';
 
 interface EnvironmentTypographyProps {
 	snippets: Snippet[];
@@ -8,26 +10,25 @@ interface EnvironmentTypographyProps {
 }
 
 export function EnvironmentTypography({ snippets, displayVariableNames, typographyProps }: EnvironmentTypographyProps) {
+	const settings = useSelector(selectSettings);
+	displayVariableNames = displayVariableNames ?? settings.displayVariableNames;
 	return (
 		<Typography {...typographyProps}>
 			{snippets.map((snippet, index) => {
-				if (snippet.variableName) {
-					const valueText = snippet.value ?? 'unknown';
-					const shouldDisplayVariable = displayVariableNames || snippet.value == null;
-					const displayText = shouldDisplayVariable ? `${snippet.variableName}: ${valueText}` : valueText;
-					return (
-						<Typography
-							variant="outlined"
-							color={snippet.value ? 'success' : 'danger'}
-							key={index}
-							sx={{ overflowWrap: 'break-word' }}
-						>
-							{displayText}
-						</Typography>
-					);
-				} else {
-					return snippet.value;
-				}
+				if (snippet.variableName == null) return snippet.value;
+				const valueText = snippet.value ?? 'unknown';
+				const shouldDisplayVariable = displayVariableNames || snippet.value == null;
+				const displayText = shouldDisplayVariable ? `${snippet.variableName}: ${valueText}` : valueText;
+				return (
+					<Typography
+						variant="outlined"
+						color={snippet.value ? 'success' : 'danger'}
+						key={index}
+						sx={{ overflowWrap: 'break-word' }}
+					>
+						{displayText}
+					</Typography>
+				);
 			})}
 		</Typography>
 	);
