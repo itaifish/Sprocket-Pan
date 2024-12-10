daa40e7bb4e90a4414bf70511e7b3034d312fedc
KateLynn Pullen <kdpullen95@gmail.com>
2024-12-10 08:48:24 -0500

    w???

--- src/components/shared/EnvironmentTypography.tsx
+++ src/components/shared/EnvironmentTypography.tsx
@@ -3,6 +3,7 @@ import { Snippet } from '../../managers/EnvironmentContextResolver';
 import { selectSettings } from '../../state/active/selectors';
 import { useSelector } from 'react-redux';
 import { VARIABLE_NAME_DISPLAY } from '../../types/settings/settings';
+import { SprocketTooltip } from './SprocketTooltip';
 
 interface EnvironmentTypographyProps {
 	snippets: Snippet[];
@@ -12,24 +13,31 @@ interface EnvironmentTypographyProps {
 
 export function EnvironmentTypography({ snippets, displayVariableNames, typographyProps }: EnvironmentTypographyProps) {
 	const settings = useSelector(selectSettings);
-	const shouldDisplayVariableNames =
-		(displayVariableNames ?? settings.interface.variableNameDisplay) === VARIABLE_NAME_DISPLAY.before;
+	const displaySetting = displayVariableNames ?? settings.interface.variableNameDisplay;
+	const shouldDisplayVariableNames = displaySetting === VARIABLE_NAME_DISPLAY.before;
+	const shouldHoverVariableNames = displaySetting === VARIABLE_NAME_DISPLAY.hover;
 	return (
 		<Typography {...typographyProps}>
 			{snippets.map((snippet, index) => {
-				if (snippet.variableName == null) return snippet.value;
+				if (snippet.variableName == null) return <span key={index}>{snippet.value}</span>;
 				const valueText = snippet.value ?? 'unknown';
 				const shouldDisplayVariable = shouldDisplayVariableNames || snippet.value == null;
 				const displayText = shouldDisplayVariable ? `${snippet.variableName}: ${valueText}` : valueText;
 				return (
-					<Typography
-						variant="outlined"
-						color={snippet.value ? 'success' : 'danger'}
+					<SprocketTooltip
 						key={index}
-						sx={{ overflowWrap: 'break-word' }}
+						text={snippet.variableName}
+						disabled={!shouldHoverVariableNames || snippet.value == null}
 					>
-						{displayText}
-					</Typography>
+						<Typography
+							component="span"
+							variant="outlined"
+							color={snippet.value == null ? 'danger' : 'success'}
+							sx={{ overflowWrap: 'break-word' }}
+						>
+							{displayText}
+						</Typography>
+					</SprocketTooltip>
 				);
 			})}
 		</Typography>
