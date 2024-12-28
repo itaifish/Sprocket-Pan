563b4375701b094ce950cd1cbf82de4074d4b658
Itai Fish <chessandpotatoes@gmail.com>
2024-12-28 14:33:19 -0500

    this is better

--- src-tauri/src/commands/save_files.rs
+++ src-tauri/src/commands/save_files.rs
@@ -11,17 +11,14 @@ pub struct FileData {
 
 #[tauri::command]
 pub async fn save_files(data: Vec<FileData>) -> bool {
-	let errors = Arc::new(Mutex::new(Vec::new()));
-	let _res: Vec<_> = data
+	let mut errors = Vec::new();
+	data
 		.par_iter()
 		.enumerate()
 		.map(|(_, file_data)| {
 			fs::write(&file_data.path, &file_data.contents)
 		})
-		.filter_map(|r| 
-			r.map_err(|e| 
-				errors.lock().unwrap().push(e)).ok())
-		.collect();
-	let no_failures = errors.lock().unwrap().len() == 0;
+		.collect_into_vec(&mut errors);
+	let no_failures = errors.len() == 0;
 	no_failures
 }
